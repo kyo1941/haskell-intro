c642c00e0993267249e699405cd72235b49a55d4
kyo1941 <kyo1941@outlook.jp>
2026-05-01 19:25:36 +0900

    練習問題を解いた

--- app/lesson03-lists/Exercises.hs
+++ app/lesson03-lists/Exercises.hs
@@ -10,13 +10,13 @@ test1 = print scores
 
 -- 問題2
 double :: Int -> Int
-doubleAll :: [Int]
+doubleAll :: [Int] -> [Int]
 double x = x * 2
-doubleAll = map double scores
+doubleAll = map double
 
 test2 :: IO ()
 test2 = do
-    print doubleAll
+    print (doubleAll scores)
 
 
 -- 問題3
@@ -43,8 +43,62 @@ test4 = do
     print shortsWords
 
 
+-- 問題5
+countScores :: [Int] -> Int
+countScores = length
+
+test5 :: IO ()
+test5 = do
+    print (countScores scores)
 
 
+-- 問題6
+-- multipleTypesList :: [Int]
+-- multipleTypesList = [1, 2, "three"]
+
+-- 出力エラー
+-- • Couldn't match type ‘[Char]’ with ‘Int’
+--     Expected: Int
+--     Actual: String
+-- • In the expression: "three"
+--     In the expression: [1, 2, "three"]
+--     In an equation for ‘multipleTypesList’:
+--         multipleTypesList = [1, 2, "three"]
+
+
+-- 問題7
+-- sampleList :: [Int]
+-- outputList :: [Int]
+-- sampleFilter :: Int -> Int
+
+-- sampleList = [1, 2, 3, 4, 5]
+-- sampleFilter x = x * 2
+
+
+-- outputList = filter sampleFilter sampleList
+
+-- 出力エラー (filter は Bool が返り値であることを期待している)
+-- • Couldn't match type ‘Int’ with ‘Bool’
+--     Expected: Int -> Bool
+--     Actual: Int -> Int
+-- • In the first argument of ‘filter’, namely ‘sampleFilter’
+--     In the expression: filter sampleFilter sampleList
+--     In an equation for ‘outputList’:
+--         outputList = filter sampleFilter sampleList
+
+
+-- 問題8
+myScore :: [Int]
+isFullScore :: Int -> Bool
+emphasizeFullScore :: Int -> String
+
+myScore = [100, 95, 40, 100, 100, 80, 75, 100]
+isFullScore score = score == 100
+emphasizeFullScore score = show score ++ "!"
+
+test8 :: IO ()
+test8 = do
+    print (map emphasizeFullScore (filter isFullScore myScore))
 
 
 main :: IO ()
@@ -53,3 +107,5 @@ main = do
     test2
     test3
     test4
+    test5
+    test8
